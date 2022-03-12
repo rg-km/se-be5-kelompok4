f98b6926519d7852bdca3848689589a46194cddd
Yoga Cahya R <yogacahya08@gmail.com>
2022-03-12 20:52:14 +0700

    added level

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Some Problems on Level Design Requirement :
+// 1. Apabila pindah level dan posisi obstacle tepat di kepala/badan, apakah mengurangi nyawa atau direset saja?
+
 const CELL_SIZE = 20;
-// Soal no 1: Set canvas size menjadi 600
 const CANVAS_SIZE = 600;
 const REDRAW_INTERVAL = 50;
 const WIDTH = CANVAS_SIZE / CELL_SIZE;
@@ -10,8 +12,62 @@ const DIRECTION = {
     UP: 2,
     DOWN: 3,
 }
-// Soal no 2: Pengaturan Speed (semakin kecil semakin cepat) ubah dari 150 ke 120
-const MOVE_INTERVAL = 120;
+
+var moveInterval = 120;
+
+// Indikator untuk menunjukkan sekarang level berapa
+var currLevel = 1;
+
+// Array untuk desain level
+const levelDesign = [
+    // Level 1
+    [],
+    // level 2
+    [
+        // Setiap array digunakan untuk menunjukkan lokasi titik antar garis
+        // Semisal jika hanya butuh untuk membuat 1 garis lurus, maka hanya perlu membuat 1 array dengan isi [x1,y1,x2,y2]
+        // Jika ingin membuat 2 garis lurus, maka buat 2 array
+        // Format : [x1,y1,x2,y2,...]
+        [
+            100, 210, 500, 210
+        ]
+    ],
+    // level 3
+    [
+        [
+            100, 210, 500, 210
+        ],
+        [
+            100, 310, 500, 310
+        ]
+    ],
+    // level 4
+    [
+        [
+            100, 210, 500, 210
+        ],
+        [
+            100, 310, 500, 310
+        ],
+        [
+            100, 410, 500, 410
+        ]
+    ],
+    // level 5
+    [
+        [
+            90, 60, 90, 540
+        ],
+        [
+            510, 60, 510, 540
+        ]
+    ]
+]
+
+function changeLevelDisplay() {
+    document.getElementById('curr-level').innerHTML = currLevel
+    document.getElementById('curr-speed').innerHTML = moveInterval
+}
 
 function initPosition() {
     return {
@@ -22,7 +78,10 @@ function initPosition() {
 
 function initHeadAndBody() {
     let head = initPosition();
-    let body = [{x: head.x, y: head.y}];
+    let body = [{
+        x: head.x,
+        y: head.y
+    }];
     return {
         head: head,
         body: body,
@@ -44,13 +103,14 @@ function initSnake(color) {
 let snake1 = initSnake("purple");
 
 let apples = [{
-    color: "red",
-    position: initPosition(),
-},
-{
-    color: "green",
-    position: initPosition(),
-}]
+        color: "red",
+        position: initPosition(),
+    },
+    {
+        color: "green",
+        position: initPosition(),
+    }
+]
 
 function drawCell(ctx, x, y, color) {
     ctx.fillStyle = color;
@@ -70,13 +130,67 @@ function drawScore(snake) {
     scoreCtx.fillText(snake.score, 10, scoreCanvas.scrollHeight / 2);
 }
 
+function drawObstacle(ctx) {
+    ctx.beginPath();
+    let levelDes = levelDesign[currLevel - 1];
+    ctx.strokeStyle = 'black';
+    for (let i = 0; i < levelDes.length; i++) {
+        for (let j = 0; j < levelDes[i].length - 2; j += 2) {
+            ctx.moveTo(levelDes[i][j], levelDes[i][j + 1]);
+            ctx.lineTo(levelDes[i][j + 2], levelDes[i][j + 3]);
+            ctx.lineWidth = CELL_SIZE;
+            ctx.stroke();
+        }
+    }
+}
+
+function checkSnakeObstacleCol(snake) {
+    let isCollide = false;
+    let levelDes = levelDesign[currLevel - 1];
+    for (let i = 0; i < levelDes.length; i++) {
+        for (let j = 0; j < levelDes[i].length - 2; j += 2) {
+            if (snake.head.x * CELL_SIZE + CELL_SIZE / 2 >= levelDes[i][j] &&
+                snake.head.x * CELL_SIZE + CELL_SIZE / 2 <= levelDes[i][j + 2] &&
+                snake.head.y * CELL_SIZE + CELL_SIZE / 2 >= levelDes[i][j + 1] &&
+                snake.head.y * CELL_SIZE + CELL_SIZE / 2 <= levelDes[i][j + 3]) {
+                console.log("Snake head x = " + snake.head.x)
+                console.log("Snake head y = " + snake.head.y)
+                // snake.body = [{
+                //     x: snake.head.x,
+                //     y: snake.head.y
+                // }]
+                gameOver();
+                isCollide = true;
+            }
+        }
+    }
+    return isCollide;
+}
+
+// Kadang apel akan spawn di obstacle, maka dibuat function ini untuk mengecek lokasi apel apakah bertabrakan
+function checkAppleObstacleCol(apple) {
+    let levelDes = levelDesign[currLevel - 1];
+    for (let i = 0; i < levelDes.length; i++) {
+        for (let j = 0; j < levelDes[i].length - 2; j += 2) {
+            if (apple.position.x * CELL_SIZE + CELL_SIZE / 2 >= levelDes[i][j] &&
+                apple.position.x * CELL_SIZE + CELL_SIZE / 2 <= levelDes[i][j + 2] &&
+                apple.position.y * CELL_SIZE + CELL_SIZE / 2 >= levelDes[i][j + 1] &&
+                apple.position.y * CELL_SIZE + CELL_SIZE / 2 <= levelDes[i][j + 3]) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
 function draw() {
-    setInterval(function() {
+    initGame();
+    setInterval(function () {
         let snakeCanvas = document.getElementById("snakeBoard");
         let ctx = snakeCanvas.getContext("2d");
 
         ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-        
+        drawObstacle(ctx);
         drawCell(ctx, snake1.head.x, snake1.head.y, snake1.color);
         for (let i = 1; i < snake1.body.length; i++) {
             drawCell(ctx, snake1.body[i].x, snake1.body[i].y, snake1.color);
@@ -113,9 +227,29 @@ function eat(snake, apples) {
     for (let i = 0; i < apples.length; i++) {
         let apple = apples[i];
         if (snake.head.x == apple.position.x && snake.head.y == apple.position.y) {
-            apple.position = initPosition();
             snake.score++;
-            snake.body.push({x: snake.head.x, y: snake.head.y});
+            snake.body.push({
+                x: snake.head.x,
+                y: snake.head.y
+            });
+            if (snake.score % 5 == 0 && currLevel < 5) {
+                var audio = new Audio('assets/sound/level-up.mp3');
+                audio.play();
+                alert(`Level ${currLevel} complete`);
+                currLevel++;
+                moveInterval -= 20;
+                changeLevelDisplay();
+                apples.forEach((e)=>{
+                    if(checkAppleObstacleCol(e)){
+                        do {
+                            e.position = initPosition();
+                        } while (checkAppleObstacleCol(e))
+                    }
+                })
+            }
+            do {
+                apple.position = initPosition();
+            } while (checkAppleObstacleCol(apple))
         }
     }
 }
@@ -156,16 +290,23 @@ function checkCollision(snakes) {
         }
     }
     if (isCollide) {
-        var audio = new Audio('game-over.mp3');
-        audio.play();
-
-        alert("Game over");
-        snake1 = initSnake("purple");
+        gameOver();
     }
     return isCollide;
 }
 
+function gameOver() {
+    var audio = new Audio('assets/sound/game-over.mp3');
+    audio.play();
+
+    alert("Game over");
+    snake1 = initSnake("purple");
+    changeLevelDisplay();
+}
+
 function move(snake) {
+    console.log("Snake head x = " + snake.head.x)
+    console.log("Snake head y = " + snake.head.y)
     switch (snake.direction) {
         case DIRECTION.LEFT:
             moveLeft(snake);
@@ -181,18 +322,27 @@ function move(snake) {
             break;
     }
     moveBody(snake);
-    // Soal no 6: Check collision dengan snake3
+
     if (!checkCollision([snake1])) {
-        setTimeout(function() {
+        setTimeout(function () {
             move(snake);
-        }, MOVE_INTERVAL);
+        }, moveInterval);
+    } else {
+        initGame();
+    }
+
+    if (!checkSnakeObstacleCol(snake1)) {
+
     } else {
         initGame();
     }
 }
 
 function moveBody(snake) {
-    snake.body.unshift({ x: snake.head.x, y: snake.head.y });
+    snake.body.unshift({
+        x: snake.head.x,
+        y: snake.head.y
+    });
     snake.body.pop();
 }
 
@@ -224,6 +374,7 @@ document.addEventListener("keydown", function (event) {
 
 function initGame() {
     move(snake1);
+    moveInterval = 120;
+    currLevel = 1;
+    changeLevelDisplay();
 }
-
-initGame();
